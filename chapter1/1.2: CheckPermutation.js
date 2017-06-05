6a693167afdba8356544b079e425f1e5cff68b56
La-Keisha Towner <ltowner622@gmail.com>
2017-06-05 13:37:17 -0400

    added solution for 1.2 Permutation

--- chapter1/1.2: CheckPermutation.js
+++ chapter1/1.2: CheckPermutation.js
@@ -1,1 +1,15 @@
-/**/
+/* Check Permutation: Given two strings, write a method to decide if one is a
+permuation of the other*/
+
+/* Psuedocode
+The strings must must have the same length, and when sorted they must be equal
+as well
+*/
+function isPermutation(string1, string2) {
+    if ((string1.length != string2.length) &&
+    (string1.split('').sort) === (string2.split('').sort)) {
+        return false
+    } else {
+        return true
+    }
+}
