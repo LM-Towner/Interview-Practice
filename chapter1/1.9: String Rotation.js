47fe873a0124c189520081be68c2634a764a11c0
La-Keisha Towner <ltowner622@gmail.com>
2017-06-06 10:46:17 -0400

    added problem

---
+++ chapter1/1.9: String Rotation.js
@@ -0,0 +1,6 @@
+/*
+String Rotation: Assume yu have method isSubstring which checks if one word is
+a substring of another. Given two strings, s1, s2, write code to check if s2 is
+a rotation of s1 using only call(eg. 'waterbottle' is a rotation of 'erbottlewat')
+
+*/
