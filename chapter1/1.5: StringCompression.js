cdbe44d7794afa4584a5d914eb1a74304ddbfeea
La-Keisha Towner <ltowner622@gmail.com>
2017-06-06 10:46:07 -0400

    added problem

---
+++ chapter1/1.5: StringCompression.js
@@ -0,0 +1,18 @@
+/*
+Implement a method to perform basic string compression using the counts of
+repeated characters. For Example, the string aabcccaaa would become a2b1c3a.
+If the compressed string string would not become smaller than the original
+ string, your method should return the original string. You can assume the
+  string has only uppercase and lowercase letters(a-z)
+*/
+
+// function stringCompression(string) {
+//     newString = string.split('');
+//     holder = [];
+//
+//     for (var i = 0; i < newString.length; i++) {
+//         for (var j = 0; j < newString.length; j++) {
+//           if(newString[i] == newString[j])
+//         }
+//     }
+// }
