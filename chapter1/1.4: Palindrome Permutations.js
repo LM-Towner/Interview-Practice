cf844986db91eecdc2a20820d278892ff1634a8a
La-Keisha Towner <ltowner622@gmail.com>
2017-06-06 10:45:50 -0400

    added problem

---
+++ chapter1/1.4: Palindrome Permutations.js
@@ -0,0 +1,9 @@
+/*Given a string, write a function to check if it is a permuation of a palidrome
+A palidrome is a word or phrase that is the same forwards and backwards. A Permutation
+is a rearragment of letters. The palidrome does not need to be
+limitied to just dictionary words
+
+Example
+Input: "Tact Coa"
+Output: True (Permutations: "taco cat", "atco cta", "atco cta")
+*/
